0079e5ab2bcfe6f8bd2c91c68369da14fead963c
Q321654w <Q321654w@gmail.com>
2021-10-05 20:45:16 +0200

    FirstBuild

--- Assets/Scripts/Game/GameTest.cs
+++ Assets/Scripts/Game/GameTest.cs
@@ -8,12 +8,19 @@ namespace Game
     {
         [SerializeField] private GameData _gameData;
         [SerializeField] private CinemachineVirtualCamera _cinemachine;
+        [SerializeField]
         private Game _game;
 
         private void Awake()
         {
             _game = new Game(_gameData);
             _cinemachine.Follow = _game.Player.CurrentBro.transform;
+        }
+
+
+        private void Update()
+        {
+            
         }
     }
 }
